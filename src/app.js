78f0c02113c0612d0984b777941cd725e1b2b921
romerolaia <93878330+romerolaia@users.noreply.github.com>
2021-12-31 18:07:38 +0100

    icon updated

--- src/app.js
+++ src/app.js
@@ -13,6 +13,13 @@ function displayTemperature(response) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let date = new Date();
